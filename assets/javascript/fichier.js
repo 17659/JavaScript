bca1a328dd1b90c55f47ebf277bd8221cd564484
Exposito Esteban <17659@csmrouen.net>
2019-02-04 12:01:10 +0100

    if esle if esle

---
+++ assets/javascript/fichier.js
@@ -0,0 +1,13 @@
+var message = prompt('Veuillez saisir une valeur');
+
+if (message < 0) {reponse = 'Nombre negatif';
+}
+
+else if (message < 0) {
+  reponse ='Nombre positif';
+}
+
+else {reponse = 'Nombre positif';
+}
+
+alert(reponse);
