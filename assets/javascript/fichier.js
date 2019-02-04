d52fa6686eb74c16a7a1e1a12929882555fd52c8
Exposito Esteban <17659@csmrouen.net>
2019-02-04 12:15:16 +0100

    document.write

--- assets/javascript/fichier.js
+++ assets/javascript/fichier.js
@@ -3,11 +3,13 @@ var message = prompt('Veuillez saisir une valeur');
 if (message < 0) {reponse = 'Nombre negatif';
 }
 
-else if (message < 0) {
+else if (message > 0) {
   reponse ='Nombre positif';
 }
 
-else {reponse = 'Nombre positif';
+else {reponse = 'Null';
 }
+
+document.write("<h1>" + reponse + "</h1>");
 
 alert(reponse);
